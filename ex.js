c82b01d5f9b5663d93c338d1e3cdd8be00967129
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2024-01-15 08:06:57 +0300

    Digging into Nodejs

---
+++ ex.js
@@ -0,0 +1,11 @@
+#! usr/bin/env node
+"use strict";
+
+
+var argv = require("minimist")(process.argv.slice(2));
+
+require("path");
+console.log(__dirname);
+// process.stdout.write("Hello World");
+
+// console.log(argv);
