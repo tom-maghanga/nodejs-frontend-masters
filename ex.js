9203359ed6edd4db9b081735278967a241672596
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2024-01-15 09:17:26 +0300

    Reading sychrounous message

--- ex.js
+++ ex.js
@@ -1,11 +1,41 @@
 #! usr/bin/env node
 "use strict";
 
+var path = require("path");
+var fs = require("fs");
 
-var argv = require("minimist")(process.argv.slice(2));
+// printhelp();
+function printhelp(){
+    console.log("ex usage : ");
+    console.log("ex.js --file{FILENAME}" )
+    console.log("  --help           Print this help");
+    console.log("--file{FILENAME}   proccess this file");
 
-require("path");
-console.log(__dirname);
-// process.stdout.write("Hello World");
+}
 
-// console.log(argv);
+var args = require('minimist')(process.argv.slice(2), {
+    boolean: 'help',
+    string : 'file'
+});
+if(args.help){
+    console.log("");
+    printhelp();
+}else if(args.file){
+    // console.log(__dirname);
+   fileReader(path.resolve(args.file));
+}else{
+    argsError("Error !, Please review", true);
+}
+
+function argsError(msg, includeFile=false){
+    console.log(msg);
+    if(includeFile){
+        console.log("");
+        printhelp();
+    }
+}
+
+function fileReader(filepath){
+    var contents = fs.readFileSync(filepath);
+    process.stdout.write(contents);
+}
